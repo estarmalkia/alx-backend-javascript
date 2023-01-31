59131450939d9a440a4a4fe63e1982853730e3bd
estarmalkia <estarmalkia>
2023-02-01 01:04:27 +0300

    create iterator object js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,10 @@
 export default function createIteratorObject(report) {
-    return [].concat(...Object.values(report.allEmployees));
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }());
 }
 
