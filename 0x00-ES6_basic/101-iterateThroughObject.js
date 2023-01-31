e34c10ff2437811c340ad9604ce26d6ec608dc56
estarmalkia <estarmalkia>
2023-02-01 01:37:42 +0300

    iterate through object js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let employeeString = "";
-    for (const { employee } of reportWithIterator) {
-        employeeString += employee + " | ";
-    }
-    return employeeString.slice(0, -3);
+  const employees = [];
+
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
+  }
+
+  return employees.join(' | ');
 }
