f38e62a976e6e8851f40bec4e76fc806c1efa47e
estarmalkia <estarmalkia>
2023-02-03 00:21:28 +0300

    0x04

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,9 @@
+import {RowElement, RowID} from "./interface";
+
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowId: RowID): number;
+declare function updateRow(rowId: RowID, row: RowElement): number;import {RowElement, RowID} from "./interface";
+
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowId: RowID): number;
+declare function updateRow(rowId: RowID, row: RowElement): number;
